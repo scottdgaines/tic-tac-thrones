4f3b9d9f467ccb62a4dd6ee67878b8765ebd31d1
Scott Gaines <scottdgaines@gmail.com>
2022-08-03 11:21:54 -0600

    Add global variables

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 //HTML querySelectors
 
-
 //Global Variables
+var player1 = new Player({name: "House Stark"});
+var player2 = new Player({name: "House Lannister"});
 
+var newGame = new Game(player1, player2)
 
+console.log(player1)
+console.log(player2)
+console.log(newGame)
 //Event Listners
 
 
