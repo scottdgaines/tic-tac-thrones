297b95c1be7cc54b15f991479606325136780370
Scott Gaines <scottdgaines@gmail.com>
2022-08-04 14:29:52 -0600

    Add tiles arrays to each Class

--- Player.js
+++ Player.js
@@ -2,6 +2,7 @@ class Player {
   constructor(player) {
     this.name = player.name,
     this.id = player.id,
-    this.wins = player.wins
+    this.wins = player.wins,
+    this.tiles = []
   }
 }
