3b8050d91ce84ec4d6701ceb54276b6588664a93
Scott Gaines <scottdgaines@gmail.com>
2022-08-05 16:43:46 -0600

    Add function to place player's logo on selected tile

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ var player1 = new Player({
   name: 'House Stark',
   id: 'one',
   wins: 0,
-  logo: 'assets/stark.png'
+  logo: 'assets/stark-white.png'
 })
 var player2 = new Player({
   name: 'House Lannister',
   id: 'two',
   wins: 0,
-  logo: 'assets/lannister.png'
+  logo: 'assets/lannister-white.png'
 })
 var currentGame = new Game(player1, player2)
 console.log(currentGame)
@@ -25,14 +25,16 @@ tiles.forEach(function (i) {
 //Event Handlers
 function selectTile(event) {
   var selection = event.target.getAttribute('id');
-  verifyTile(currentGame, selection);
+  var selectedElement = event.target
+  verifyTile(currentGame, selection, selectedElement);
   return selection;
 }
 
-function verifyTile(currentGame, selection) {
+function verifyTile(currentGame, selection, selectedElement) {
   if (!currentGame.p1Tiles.includes(selection) &&
     !currentGame.p2Tiles.includes(selection) ) {
     logSelectedTile(selection, currentGame);
+    placeLogo(currentGame, selectedElement);
     togglePlayer(currentGame, player1, player2);
   }
 }
@@ -41,6 +43,7 @@ function logSelectedTile(selection, currentGame) {
   console.log(currentGame.currentTurn.name)
   currentGame.currentTurn.tiles.push(selection);
   console.log(currentGame.currentTurn.tiles)
+  console.log(currentGame.currentTurn.logo)
 }
 
 function togglePlayer(currentGame, p1, p2) {
@@ -50,3 +53,9 @@ function togglePlayer(currentGame, p1, p2) {
   currentGame.currentTurn = p1
   }
 }
+
+function placeLogo(currentGame, selectedElement) {
+  selectedElement.innerHTML = `<img class="tile-icon" src="${currentGame.currentTurn.logo}"
+ alt="${currentGame.currentTurn.name}\'s logo" />`
+ console.log(selectedElement)
+};
