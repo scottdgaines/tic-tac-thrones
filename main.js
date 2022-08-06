b9294ed2df10c31fc4ed5de964458f21dbd5eeb6
Scott Gaines <scottdgaines@gmail.com>
2022-08-05 19:00:36 -0600

    Add clarifying comments

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ var player1 = new Player({
   id: 'one',
   wins: 0,
   logo: 'assets/stark-white.png'
-})
+});
 var player2 = new Player({
   name: 'House Lannister',
   id: 'two',
   wins: 0,
   logo: 'assets/lannister-white.png'
-})
-var currentGame = new Game(player1, player2)
+});
+var currentGame = new Game(player1, player2);
 console.log(currentGame)
 
 //Event Listeners
@@ -24,6 +24,7 @@ tiles.forEach(function (i) {
 });
 
 //Event Handlers
+//pulls ID from selected tile to be used in following fxns
 function selectTile(event) {
   var selection = event.target.getAttribute('id');
   var selectedElement = event.target
@@ -31,6 +32,7 @@ function selectTile(event) {
   return selection;
 }
 
+//Keeps an occupied tile from being reassigned
 function verifyTile(currentGame, selection, selectedElement) {
   if (!currentGame.p1Tiles.includes(selection) &&
     !currentGame.p2Tiles.includes(selection) ) {
@@ -39,14 +41,14 @@ function verifyTile(currentGame, selection, selectedElement) {
     togglePlayer(currentGame, player1, player2, banner);
   }
 }
-
+//Records selected tile ID in the selecting-player's array
 function logSelectedTile(selection, currentGame) {
   console.log(currentGame.currentTurn.name)
   currentGame.currentTurn.tiles.push(selection);
   console.log(currentGame.currentTurn.tiles)
   console.log(currentGame.currentTurn.logo)
 }
-
+//Changes whose turn it is, updates banner and grid icon
 function togglePlayer(currentGame, p1, p2, banner) {
   if (currentGame.currentTurn === p1) {
   currentGame.currentTurn = p2;
@@ -56,7 +58,7 @@ function togglePlayer(currentGame, p1, p2, banner) {
   banner.innerText = ` ${p1.name}`
   }
 }
-
+//Changes selected tile to logo of current player
 function placeLogo(currentGame, selectedElement) {
   selectedElement.innerHTML = `<img class="tile-icon" src="${currentGame.currentTurn.logo}"
  alt="${currentGame.currentTurn.name}\'s logo" />`
