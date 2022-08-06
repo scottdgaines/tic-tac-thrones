9505f6208be20f699ed04f12cef7b71fb7341c21
Scott Gaines <scottdgaines@gmail.com>
2022-08-05 18:45:49 -0600

    Add feature to toggle current player banner

--- Game.js
+++ Game.js
@@ -1,10 +1,10 @@
 class Game {
-  constructor(p1, p2) {
+  constructor(p1, p2, banner) {
     this.p1wins = p1.wins,
     this.p1Tiles = p1.tiles,
     this.p2wins = p2.wins,
     this.p2Tiles = p2.tiles,
     this.draws = 0,
-    this.currentTurn = p1
+    this.currentTurn = p1;
   }
 }
